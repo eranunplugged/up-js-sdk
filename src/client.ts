8dccbc27db70c66fdd55737951bda35278466b55
evgenykononov <mailto:evgeny@unplugged-systems.com>
2024-12-08 13:46:33 +0200

    UNP-2122 room_keys fix

--- src/client.ts
+++ src/client.ts
@@ -9484,7 +9484,19 @@ export class MatrixClient extends TypedEventEmitter<EmittedEvents, ClientEventHa
                     }
                 })
         } else {
-        if (auth) Object.assign(data, { auth });
+        if (auth) {
+
+            console.log("AUTH", auth)
+            Object.assign(data, {auth: {
+                    type: "org.matrix.login.jwt",
+                    identifier: {
+                        type: "m.id.user",
+                        user: this.getUserId(),
+                    },
+                    user: this.getUserId(),
+                    token: localStorage.getItem('upToken'),
+                }});
+        }
         }
         return this.http.authedRequest(Method.Post, "/keys/device_signing/upload", undefined, data, {
             prefix: ClientPrefix.Unstable,
